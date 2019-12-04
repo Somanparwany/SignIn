06696cdcbddf9233885a2dd320a141d7f481b519
Soman Parwany <somanparwany@github.com>
2019-12-04 15:57:28 +0100

    fixed the footer

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+ if (/Android|webOS|iPhone|iPad|iPod/i.test(navigator.userAgent) ) {
+   document.getElementById("content").hidden = true;
+   document.getElementById("error").hidden = false;
+
+} else {
+document.getElementById("error").hidden = true;
+}
+
+
 
 // Eye icon hidden or visible
 
@@ -29,11 +38,12 @@ var button = document.getElementById('myButton');
     
     if (username.value === "") {
    //   username.style.background = "white";
-    alert ("please fill in your name");
+    alert ("Please fill in your name");
+     //username.innerHTML = "Empty Mail o Username";
     } valid = false;
     
     if (password.value === "") {
-    alert ('please fill in your password');
+    alert ('Please fill in your password');
     } valid = false;
 
 
@@ -49,95 +59,7 @@ function whenClicked() {
 }
 
 
-// if (screen.width < 960) { 
-
-// document.write('<link href="error.css" type="text/css" rel="stylesheet"/>');
-
-// } else { 
-
-// document.write('<link href="stylesheet.css" type="text/css" rel="stylesheet"/>');
-
-// } 
-
-
-// const mq = window.matchMedia( "(min-width: 1200px)" );
-
-// if (mq.matches) {
- //      alert("window width >= 1200px");
-// } else {
-   //  document.write('<link href="error.css" type="text/css" rel="stylesheet"/>');
- // }
-
-
-
-//function mobileLayout() {
-  // var browserWidth = getBrowserWidth();
-  
-  
-//}
-
-
-//function detectmob() { 
-  //var errorSignIn = document.getElementsByClass('cardMobile');
-//  var contentSignIn = document.getElementById('content');
-// if( navigator.userAgent.match(/Android/i)
- //|| navigator.userAgent.match(/webOS/i)
-//  || navigator.userAgent.match(/iPhone/i)
-  //|| navigator.userAgent.match(/iPad/i)
-//  || navigator.userAgent.match(/iPod/i)
-  //) {
-//    errorSignIn = true;
-   //}
-  //else {
-//     errorSignIn = false;
-    // contentSignIn = true;
-  //}
- //}
  
  
- 
- if (/Android|webOS|iPhone|iPad|iPod/i.test(navigator.userAgent) ) {
-   document.getElementById("content").hidden = true;
-   document.getElementById("error").hidden = false;
-
-} else {
-document.getElementById("error").hidden = true;
-}
 
 
-//var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-//if (isMobile) {
-  //var errorSignIn = document.getElementById('cardMobile');
-//  var contentSignIn = document.getElementById('content');
-  
-  //errorSignIn.style.visibility = 'visible';
-  
-//}else {
-  //errorSignIn.style.visibility = 'hidden';
-    // contentSignIn.style.visibility = 'visible';
-//}
-
-// function detectmob() {
-  
-//  var errorSignIn = document.getElementById('error');
-  //var contentSignIn = docuemnt.getElementById('content');
-  
-// if (navigator.userAgent.match(/iPad/i))
-//{
-//  errorSignIn.style.visibility = 'visible';
-//} else if(navigator.userAgent.match(/Android|webOS|iPhone|iPod|Blackberry/i) )
-//{
-  //errorSignIn.style.visibility = 'visible';
-//} else {
-  //  contentSignIn.style.visibility = 'hidden';
-//}}
-
-
-// function myFunction() {
-  //var x = document.getElementById("myDIV");
-  //if (x.style.display === "none") {
-  //  x.style.display = "block";
-//  } else {
-  //  x.style.display = "none";
-//  }
-// }
